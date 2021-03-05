7c24f2eef4835789e4397972db39d44b4bef19b2
Bram <brampijper@gmail.com>
2021-03-05 09:14:43 +0100

    Content of component is updated through strapi cms

--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useStaticQuery, graphql} from 'gatsby';
 import ButtonMailTo from './ButtonMailTo';
 
+const getData = graphql `
+query getContactBarContent {
+    strapiHomepage {
+        contactbar_text
+        contactbar_email
+      }
+}
+`
+
 const ContactBar = () => {
+    const data = useStaticQuery(getData);
+    const {
+        strapiHomepage: { contactbar_text: text, contactbar_email: email}
+    } = data;
+
     return (
         <>
-            <ButtonMailTo email="hello@sofia.com" styles="w-screen h-28 md:h-12 bg-s-turquoise flex justify-center items-center cursor-pointer">
+            <ButtonMailTo email={email} styles="w-screen h-28 md:h-12 bg-s-turquoise flex justify-center items-center cursor-pointer">
                 <p className="text-white text-center flex flex-col md:flex-row ">
-                    <span>Contact me now:&nbsp;</span> 
-                    <span>kakkava.sofia@gmail.com</span>
+                    <span>{text}&nbsp;</span> 
+                    <span>{email}</span>
                 </p>
             </ButtonMailTo>
         </>
