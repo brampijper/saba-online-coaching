f8a1c11436312238cbd86e6c264f732341a6027e
Bram <brampijper@gmail.com>
2021-03-10 20:27:40 +0100

    added defaultprops

--- src/components/ButtonMailTo.js
+++ src/components/ButtonMailTo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ButtonMailTo = ({ email, styles, children }) => {
+
     return (
             <a  href={'mailto:' + email}
                 className={styles}
@@ -9,5 +10,16 @@ const ButtonMailTo = ({ email, styles, children }) => {
             </a>
     )
 }
+
+// Set default props
+ButtonMailTo.defaultProps = {
+    email: "hello@sofiakakkava.com",
+    children: "Send a message",
+    styles: `
+        mb-5 mt-5 
+        bg-white border-2 rounded border-black 
+        w-full px-6 py-2.5 
+        text-center`
+  };
 
 export default ButtonMailTo;
