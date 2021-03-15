bc3311fa0833a54ffc427aec602b2345d33613f4
Bram <brampijper@gmail.com>
2021-03-15 16:08:29 +0100

    removed max width css from main component

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,11 @@ const Layout = ({children}) => {
             <ContactBar />
             <Navigation />
 
-            <main className="max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44">
+            <main className="space-y-20 
+                sm:space-y-32 
+                md:space-y-40 
+                lg:space-y-44
+            ">
                 {children}
             </main>
 
