987ee738345840b612fff8df4c4793910e4f77bc
Bram <brampijper@gmail.com>
2021-03-20 13:51:17 +0100

    expanded talwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,43 @@ module.exports = {
   theme: {
     extend: {      
       fontSize: {
-        'xs': '1.125rem',   // 18px
+        'xs': '0.85rem',  // 14px
+        'base': '1.125rem',   // 18px
         'sm': '1.375rem',   // 21px
       },
       fontFamily: {
         'averta': ['Averta', 'Open Sans'],
-        'gabriela': ['Gabriela', 'serif']
+        'gabriela': ['Gabriela', 'serif'],
       },
       colors: {
         's-turquoise': '#5EC4B6',
         's-gold': '#D2B271',
         's-purple': '#312B6E',
-        's-turquoise-light': '#D9EEE7'
+        's-turquoise-light': '#D9EEE7',
       },
       backgroundImage: theme => ({
         'blocks': "url('/images/svg/blocks.svg')",
         'stripe': "url('/images/svg/stripe.svg')",
       }),
       zIndex: {
-        '-10': '-10'
+        'minus': '-10',
       },
       inset: {
-        '1/10': '25%'
-      }
+        '1/10': '25%',
+        '1/11': '-10%',
+        '1/13': '-13%',
+        '1/24': '-24%',
+        '1/20': '-20%',
+        '1/5':  '20%',
+        '1/15': '15%',
+      },
+      minWidth: {
+        '16': '16rem',
+        '32': '32rem',
+      },
+      transitionProperty: {
+        'height': 'height',
+      },
     },
   },
   variants: {
