5cfbace9ca668fdc82e63e8faba8e4af04e88803
Bram <brampijper@gmail.com>
2021-03-20 13:54:39 +0100

    added all the components for the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,18 @@ import * as React from "react";
 import Layout from '../components/layout';
 import Header from '../components/Header';
 import AboutMe from '../components/AboutMe';
-
+import Services from '../components/Services';
+import Testimonials from '../components/Testimonials';
+import HelpSection from '../components/HelpSection';
 
  const IndexPage = () => {
   return (
     <Layout>
         <Header />
         <AboutMe />
+        <Services />
+        <Testimonials />
+        <HelpSection />
     </Layout>
   )
 }
