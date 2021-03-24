fdcc878424103ad5ba0e420a2dea457b1989b940
Bram <brampijper@gmail.com>
2021-03-24 15:42:46 +0100

    re-activated navigtion to the about page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -61,7 +61,7 @@ const AboutMe = () => {
                         
                         <div>
                             <MarkdownView markdown={aboutme_text} className="space-y-6 max-w-prose pb-4" />
-                            {/* <Link className="underline" to='/about'>Read more</Link> */}
+                            <Link className="underline" to='/about'>Read more</Link>
                         </div>
 
                         <div className="space-y-6 place-self-center">
