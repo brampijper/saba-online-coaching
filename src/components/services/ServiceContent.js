faf03aa3394fbe961ef25fde1cff36a6d06dac7b
Bram <brampijper@gmail.com>
2021-03-24 15:46:00 +0100

    deconstructed id from service content

--- src/components/services/ServiceContent.js
+++ src/components/services/ServiceContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ButtonMailTo from '../ButtonMailTo';
 
-const ServiceContent = ({ service: {service} }) => {
+const ServiceContent = ({ service: {service, id} }) => {
     let bgColor = "bg-s-turquoise-light";
     let textColor = "text-black";
     let titleColor = "text-s-purple";
@@ -11,21 +11,20 @@ const ServiceContent = ({ service: {service} }) => {
         textColor = "text-white";
         titleColor = "text-s-turquoise"
     }
-    console.log(service)
     return (
         <>
-            <div className={ service.darkMode ? "bg-s-purple pt-32 pb-32 text-white" : " " }>
+            <div className={ service.darkMode ? "bg-s-purple pt-16 pb-32 text-white" : "" }>
             
-                <div className="container mx-auto text-center h-auto grid grid-cols-1 gap-12 md:space-y-28 justify-items-center
+                <div id={id} className="container mx-auto text-center h-auto grid grid-cols-1 gap-12 md:space-y-28 justify-items-center
                 xl:max-w-screen-xl max-w-screen-lg px-6">
                     <div>
                         <h2 className="mb-6 mt-24">{service.title}</h2>
                         <p className="lg:w-2/4 mx-auto">{service.description}</p>
                     </div>
                     <div className="flex flex-col space-y-12 md:flex-row md:space-y-0 lg:space-x-40 md:space-x-20">
-                        <div className="bg-s-turquoise-light h-36 w-100 lg:h-48 lg:w-48 rounded border-2 border-s-purple flex justify-center items-center"><span className="text-xl lg:text-2xl font-bold text-s-purple word-spacing-wide">1:1 coaching</span></div>
-                        <div className="bg-s-turquoise-light h-36 w-100 lg:h-48 lg:w-48 rounded border-2 border-s-purple flex justify-center items-center"><span className="text-xl lg:text-2xl font-bold text-s-purple word-spacing-wide">{service.price}$ p/session</span></div>
-                        <div className="bg-s-turquoise-light h-36 w-100 lg:h-48 lg:w-48 rounded border-2 border-s-purple flex justify-center items-center"><span className="text-xl lg:text-2xl font-bold text-s-purple word-spacing-wide">{service.program_duration} days</span></div>
+                        <div className={` ${bgColor} h-36 w-100 lg:h-48 lg:w-48 rounded border-2 border-s-purple flex justify-center items-center`}><span className="text-xl lg:text-2xl font-bold text-s-purple word-spacing-wide">1:1 coaching</span></div>
+                        <div className={` ${bgColor} h-36 w-100 lg:h-48 lg:w-48 rounded border-2 border-s-purple flex justify-center items-center`}><span className="text-xl lg:text-2xl font-bold text-s-purple word-spacing-wide">{service.price}$ p/session</span></div>
+                        <div className={` ${bgColor} h-36 w-100 lg:h-48 lg:w-48 rounded border-2 border-s-purple flex justify-center items-center`}><span className="text-xl lg:text-2xl font-bold text-s-purple word-spacing-wide">{service.program_duration} days</span></div>
                     </div>
 
                     <div className="container grid grid-cols-1 gap-16 md:grid-cols-2 md:text-left md:gap-20 mx-auto mt-14">
