8a210e6127e08c6a804e3ef967b9e8a2b9be79f0
Bram <brampijper@gmail.com>
2021-03-26 22:06:19 +0100

    testimonial page queries all testimonials content from strapi

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import Layout from '../components/layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
-import Testimonials from '../components/Testimonials';
+import Layout from '../components/layout';
+import TestimonialCard from "../components/testimonial/TestimonialCard";
+import BgTestimonials from "../images/svg/bg_testimonials.svg";
+
+const getData = graphql`
+query getAllTestimonials {
+    allStrapiTestimonals {
+      testimonials: nodes {
+        id
+        title
+        description
+        workplace
+        name
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 
 const TestimonialPage = () => {
+    const data = useStaticQuery(getData);
+    const {
+        allStrapiTestimonals: { testimonials }
+    } = data
     return (
         <Layout>
-            <Testimonials />
+            <div className="h-auto pb-20 lg:pb-40 relative">    
+                <div className="h-auto max-w-screen-lg xl:max-w-screen-xl mx-auto px-6">
+                <h2 className="text-center pt-20">
+                        View all testimonials
+                    </h2>
+                    <div className="card-container grid grid-flow-row gap-12 place-content-center mt-32 lg:grid-cols-1 lg:gap-20">
+                        {
+                            testimonials.map( (testimonial) => (
+                                <TestimonialCard key={testimonial.id} card={testimonial} />
+                            ))
+                        }
+                    </div>
+                    <BgTestimonials className="w-full h-full absolute top-1/20 md:top-1/20 transform scale-x-150 z-minus" />
+                </div>
+            </div>
         </Layout>
     )
 }
