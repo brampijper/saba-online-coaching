22a571682ca9c7005a2f2b3f35cdf5da3e5096d2
Bram <brampijper@gmail.com>
2021-04-01 16:38:36 +0200

    extracted code for creating two components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,9 @@ import React, {useState} from 'react';
 import { Link, graphql, useStaticQuery} from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
+import NavigationMobile from './NavigationMobile';
 import OnHoverChangeText from './OnHoverChangeText';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import NavigationHamburger from './NavigationHamburger';
 
 const getNavbarContent = graphql`
 query retrieveImage {
@@ -28,6 +28,7 @@ query retrieveImage {
       }
   }
 `
+
 const Navigation = () => {
     const data = useStaticQuery(getNavbarContent);
     const [showMenu, setShowMenu] = useState(false);
@@ -51,8 +52,7 @@ const Navigation = () => {
     }
 
     return (
-        <>
-        <nav className="h-24 flex justify-between items-center px-6 relative">
+        <nav className="h-24 flex items-center justify-between px-6 relative">
 
             <Link to='/' className="z-20">
                 <OnHoverChangeText className="hidden lg:flex lg:flex-row text-black" defaultString="ofia Kakkava" taglines={taglines} />
@@ -66,38 +66,10 @@ const Navigation = () => {
                 <li><Link to='/testimonials'>Testimonials</Link></li>
             </ul>
 
-            <div className="sm:hidden z-20" role="button" tabIndex="0" onClick={handleClick}>
-                {
-                    showMenu 
-                        ? <FontAwesomeIcon icon={faTimes} className='fa-2x' />
-                        : <FontAwesomeIcon icon={faBars} className='fa-2x' />
-                }
+            <NavigationHamburger showMenu={showMenu} onClick={handleClick} />
+            <NavigationMobile showMenu={showMenu} />
 
-            </div>
-
-            <ul id="mobile-menu"
-                role="menu"
-                tabIndex="0"
-                onClick={handleClick}
-                className= 
-                {`
-                    ${showMenu ? "absolute" : "hidden"}
-                    bg-white
-                    sm:hidden 
-                    absolute left-0 top-0
-                    full-navbar-height w-full z-10
-                    text-s-turquoise text-4xl text-center
-                    flex flex-col justify-center space-y-7
-                    overscroll-y-contain
-                `}
-            >
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/services'>Services</Link></li>
-                <li><Link to='/testimonials'>Testimonials</Link></li>
-            </ul>
         </nav>
-        </>
     )
 }
 
