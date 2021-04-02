17507a0a87b21dd9405f1e1b22e2dca62c2ae040
Bram <brampijper@gmail.com>
2021-04-02 15:52:14 +0200

    removed unnessary elements

--- src/components/homepage/TestimonialSection.js
+++ src/components/homepage/TestimonialSection.js
@@ -37,28 +37,24 @@ const TestimonialSection = () => {
         }
     } = data
     return (
-        <>
-        <div>
-            <div className="h-auto pb-20 lg:pb-40 relative">    
-                <div className="h-auto max-w-screen-lg xl:max-w-screen-xl mx-auto px-6">
-                    <h2 className="text-center pt-20">
-                        {testimonial_title}
-                    </h2>
-                    <p className="text-center max-w-lg mx-auto mt-10">
-                        {testimonial_subtitle}
-                    </p>
-                    <div className="card-container grid grid-flow-row gap-12 place-content-center mt-32 lg:grid-cols-1 lg:gap-20">
-                        {
-                            testimonials.map( (card) => (
-                                <TestimonialCard key={card.id} card={card} />
-                            ))
-                        }
-                    </div> 
-                </div>
-                <BgTestimonials className="w-full h-full absolute top-1/20 md:top-0 xl:transform xl:scale-x-150 z-minus" />
+        <div className="h-auto pb-20 lg:pb-40 relative">    
+            <div className="h-auto max-w-screen-lg xl:max-w-screen-xl mx-auto px-6">
+                <h2 className="text-center pt-20">
+                    {testimonial_title}
+                </h2>
+                <p className="text-center max-w-lg mx-auto mt-10">
+                    {testimonial_subtitle}
+                </p>
+                <div className="card-container grid grid-flow-row gap-12 place-content-center mt-32 lg:grid-cols-1 lg:gap-20">
+                    {
+                        testimonials.map( (card) => (
+                            <TestimonialCard key={card.id} card={card} />
+                        ))
+                    }
+                </div> 
             </div>
+            <BgTestimonials className="w-full h-full absolute top-1/13 sm:top-0 md:top-0 xl:transform xl:scale-x-200 z-minus" />
         </div>
-        </>
     )
 }
 
