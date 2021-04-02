0ef576671e06ac824ab2fc361d60dd70855c54ee
Bram <brampijper@gmail.com>
2021-04-02 17:31:46 +0200

    contactbar is now full width insead of screen width

--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -19,7 +19,7 @@ const ContactBar = () => {
 
     return (
         <>
-            <ButtonMailTo email={email} styles="w-screen h-12 bg-s-turquoise flex justify-start sm:justify-center items-center cursor-pointer">
+            <ButtonMailTo email={email} styles="w-full h-12 bg-s-turquoise flex justify-start sm:justify-center items-center cursor-pointer">
                 <p className="text-white px-6 md:flex md:flex-row ">
                     <span className="hidden md:block">{text}&nbsp;</span> 
                     <span>{email}</span>
