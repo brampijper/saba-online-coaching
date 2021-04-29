8ae626a685ebdfc63d4da4ced8730f6273fc0eca
Bram <brampijper@gmail.com>
2021-04-29 11:57:33 +0200

    added meta, title and html seo tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Navigation from './Navigation';
 import Footer from './Footer';
 import ContactBar from './ContactBar';
+import { Helmet } from "react-helmet";
 
 const Layout = ({children}) => {
     return (
         <>
+            <Helmet>
+                <html lang="en" />
+                <meta name="description" content="An online coaching business by Sofia Kakkava." />
+                <title>Sofia Kakkava - Online Coaching</title>  
+            </Helmet>
             <ContactBar />
             <Navigation />
 
