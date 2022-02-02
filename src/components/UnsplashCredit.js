78e97f8f6c2716f44bcf408ea281b436bbd1044d
Bram <brampijper@gmail.com>
2022-02-02 16:48:55 +0100

    created new component to display credits from unsplash

---
+++ src/components/UnsplashCredit.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const UnsplashCredit = (props) => {
+    const url = `https://unsplash.com/${props.unsplashName}?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`
+    console.log(props.textColor)
+    return (
+        <div className={`absolute inset-x-1 bottom-2 text-xs text-black font-averta text-${props.textColor}`}>
+            By 
+            {' '}
+            <a href={url} target="_blank" rel="noreferrer">{props.photographer}</a>
+            {' '}
+            on
+            {' '}
+            <a href={url} target="_blank" rel="noreferrer">Unsplash</a>
+        </div>
+    )
+}
+
+export default UnsplashCredit;
+
