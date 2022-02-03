e3a8db105b151e1cc4e11859177ba8ebd31696b3
Bram <brampijper@gmail.com>
2022-02-03 17:33:34 +0100

    added classname as props on the unsplashcredit component

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -78,6 +78,7 @@ const Header = () => {
                             photographer="Gabnrielle Clare Marino" 
                             unsplashName="@gabiontheroad"
                             textColor="s-turquoise-light"
+                            className="absolute inset-x-1 bottom-2"
                         />
                     </div>
                 </div>
