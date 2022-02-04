95426286ed6862b2f215ca2fa9a270a8e36700ac
Bram <brampijper@gmail.com>
2022-02-04 18:35:26 +0100

    title is being displayed on about-me page

--- src/components/aboutpage/Cards.js
+++ src/components/aboutpage/Cards.js
@@ -7,6 +7,7 @@ const getData = graphql`
 query GetAboutPageContent {
     strapiAboutPage {
       about_me
+      title
       what_ive_done
       aboutme_images {
         alternativeText
@@ -35,16 +36,18 @@ const Cards = () => {
     const data = useStaticQuery(getData);
     const {
         strapiAboutPage: {
-            about_me, what_ive_done, aboutme_images, whativedone_images
+            about_me, what_ive_done, aboutme_images, whativedone_images, title
         }
     } = data;
 
     return (
         <div className="text-center h-auto max-w-screen-lg space-y-2
                         mx-auto flex flex-col items-center
-                        xl:max-w-screen-xl lg:space-y-16">        
-            <CardsLayout text={about_me} image={aboutme_images} />
-            <CardsLayout text={what_ive_done} image={whativedone_images} order="reverse" alt={true} />
+                        xl:max-w-screen-xl lg:space-y-16"
+        >
+          <h2 className="md:my-20 md:block hidden">{title}</h2>        
+          <CardsLayout text={about_me} image={aboutme_images} />
+          <CardsLayout text={what_ive_done} image={whativedone_images} order="reverse" alt={true} />
         </div>
     )
 }
