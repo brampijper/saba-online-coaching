b035ba4e4f4252482f9aefb6ebab965d19c14ae6
Bram <brampijper@gmail.com>
2022-02-04 18:35:55 +0100

    TakeAways are implemented as a seperate component

--- src/components/aboutpage/index.js
+++ src/components/aboutpage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Cards from './Cards';
+import TakeAways from './TakeAways';
 
 const AboutPageContent = () => {
     return (
         <>
             <Cards />
+            <TakeAways />
         </>
     )
 }
