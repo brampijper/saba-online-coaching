d6b4392592c24826dc19926818a6882b097d74cb
Bram <brampijper@gmail.com>
2022-02-04 16:09:32 +0100

    Created component to show image on a card

---
+++ src/components/aboutpage/imageCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { GatsbyImage } from 'gatsby-plugin-image';
+
+import UnsplashCredit from '../UnsplashCredit';
+
+const ImageCard = (props) => {
+    return (
+        <div className={`relative ${props.className}`}>
+            <GatsbyImage key={props.id} image={props.image} alt={props.alt} className="max-h-96 rounded-lg" />
+            <UnsplashCredit 
+                    photographer="Gabnrielle Clare Marino" 
+                    unsplashName="@gabiontheroad"
+                    textColor="s-turquoise-light"
+                    className="absolute inset-x-1 bottom-2 text-left"
+                />
+        </div>
+    )
+
+
+}
+
+export default ImageCard;
