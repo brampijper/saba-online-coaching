e2f215acd916feabd403fb09b27c73439e29fcb7
Bram <brampijper@gmail.com>
2022-02-04 16:13:13 +0100

    created index file to render components for the about-me page

---
+++ src/components/aboutpage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cards from './Cards';
+
+const AboutPageContent = () => {
+    return (
+        <>
+            <Cards />
+        </>
+    )
+}
+
+export default AboutPageContent
