5e761f5c961a0bd7e31a1a214962b447a6caed88
Bram <brampijper@gmail.com>
2022-02-05 15:45:14 +0100

    deconstructed props

--- src/components/UnsplashCredit.js
+++ src/components/UnsplashCredit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const UnsplashCredit = ({textColor, className, photographer}) => {
-    const url = `https://unsplash.com/${props.unsplashName}?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`
+const UnsplashCredit = ({textColor, className, photographer, unsplashName}) => {
+    const url = `https://unsplash.com/${unsplashName}?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`
     return (
         <div className={`text-xss text-black font-averta ${textColor} ${className}`}>
             Photo by 
