c5284f9e5082be86909a405e1c3811c753b5e1c0
Bram <brampijper@gmail.com>
2022-02-05 15:04:37 +0100

    deconstructed props

--- src/components/aboutpage/TextCard.js
+++ src/components/aboutpage/TextCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import MarkdownView from 'react-showdown';
 
-const TextCard = (props) => {
+const TextCard = ({ secondaryStyle, markdown, className}) => {
     let cardStyle; 
     //change layout of card
-    props.alt   ? cardStyle = 'bg-white border-2 border-s-gold text-black' 
-                : cardStyle = 'bg-s-purple border-0 text-white';
+    secondaryStyle  ? cardStyle = 'bg-white border-2 border-s-gold text-black' 
+                    : cardStyle = 'bg-s-purple border-0 text-white';
     return (
         <article className={`p-6 md:p-12 rounded-lg text-sm space-y-8 max-w-prose order-2 ${cardStyle}`}>
-        <MarkdownView markdown={props.markdown} className={props.className} />
+        <MarkdownView markdown={markdown} className={className} />
     </article>
     )
 }
