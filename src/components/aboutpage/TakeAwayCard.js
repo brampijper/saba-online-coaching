c1b49935e9766b04c53fb7272572e1622ac213a8
Bram <brampijper@gmail.com>
2022-02-05 15:03:02 +0100

    destructured props

--- src/components/aboutpage/TakeAwayCard.js
+++ src/components/aboutpage/TakeAwayCard.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "gatsby";
 import MarkdownView from 'react-showdown';
 
-const TakeAwayCard = ({serviceID, id, title, takeaways}) => {
+const TakeAwayCard = ({serviceID, title, takeaways}) => {
     return (
-        <Link to={`/services#${serviceID}`} key={id} className="h-auto rounded-lg bg-white shadow-xl relative p-8 space-y-6">
+        <Link to={`/services#${serviceID}`} className="h-auto rounded-lg bg-white shadow-xl relative p-8 space-y-6">
             <h4>{title}</h4>
             <MarkdownView markdown={takeaways} className="max-w-prose" />
         </Link>
