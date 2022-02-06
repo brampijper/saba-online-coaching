2d6e48a292add5018093ed294646bd2377dd9df9
Bram <brampijper@gmail.com>
2022-02-06 19:27:37 +0100

    simplified graphql query

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -6,26 +6,22 @@ import Accordion from '../components/Accordion';
 
 const getData = graphql`
 query getStrapiFaqContent {
- strapiFaqPage {
-  faqs: faqtest {
-   answer
-   id
-   question
-  } 
- }
+    strapiFaqPage {
+        faqs: faqtest
+    }
 }
 `
 
 const Faq = () => {
     const data = useStaticQuery(getData);
     const {
-        strapiFaqPage: { faqs }
+        strapiFaqPage: {
+            faqs
+        }
     } = data
-
     return (
         <Layout>
             <div className="mx-auto text-center flex flex-col space-y-12 md:space-y-32 m-16 md:m-24">
-
                 <h2 className="mb-4">Frequent Questions &amp; Answers</h2>
                 <div className="px-8 flex flex-col space-y-12 h-auto max-w-screen-lg mx-auto text-left">
                     <section className="shadow">
