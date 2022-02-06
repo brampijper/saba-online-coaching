f04bf41108ec80af68b7d1a1db5ff3bce0b6fc9d
Bram <brampijper@gmail.com>
2022-02-06 16:41:57 +0100

    quote line height increased

--- src/components/homepage/HeaderQuote.js
+++ src/components/homepage/HeaderQuote.js
@@ -5,7 +5,7 @@ const HeaderQuote = ({quote}) => {
     return (
         <div className="container mx-auto">
             <div className="px-6 my-20 flex space-x-3 md:max-w-4xl mx-auto">
-                <h3 className="relative m-auto text-center">
+                <h3 className="relative m-auto text-center leading-relaxed">
                     <span className="font-extrabold	text-3xl mx-1 md:text-5xl font-bold font-gabriela">"</span>
                         {quote}
                     <span className="font-extrabold	text-3xl mx-1 md:text-5xl font-bold font-gabriela">"</span>
