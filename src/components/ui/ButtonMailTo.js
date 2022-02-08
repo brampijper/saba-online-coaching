90247f4fe6ba496eb21112492092f65c3ff2cb41
Bram <brampijper@gmail.com>
2022-02-08 15:51:33 +0100

    swapped children props for buttontext

--- src/components/ui/ButtonMailTo.js
+++ src/components/ui/ButtonMailTo.js
@@ -1,25 +1,22 @@
 import React from 'react';
 
-const ButtonMailTo = ({ email, styles, children }) => {
-
+const ButtonMailTo = ({ email, styles, buttonText }) => {
     return (
-            <a  href={'mailto:' + email}
-                className={styles}
-            >
-                {children}
-            </a>
+        <a  href={'mailto:' + email} className={styles}>
+            {buttonText}
+        </a>
     )
 }
 
 // Set default props
 ButtonMailTo.defaultProps = {
     email: "Saba.Feronah@gmail.com",
-    children: "Send a message",
+    buttonText: "Send a message",
     styles: `
         mb-5 mt-5 
         bg-white border-2 rounded border-black 
         max-w-md px-6 py-2.5 
-        text-center
+        text-center text-black
         md:max-w-xs
         `
   };
