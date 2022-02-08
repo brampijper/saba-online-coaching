db2d7f52ac52acba25792c30be9a7c002c84113e
Bram <brampijper@gmail.com>
2022-02-08 18:23:06 +0100

    removed unnessary tailwind classes

--- src/components/homepage/HeaderQuote.js
+++ src/components/homepage/HeaderQuote.js
@@ -3,16 +3,16 @@ import ArrowBounceDown from '../ui/ArrowBounceDown';
 
 const HeaderQuote = ({quote}) => {
     return (
-        <div className="container mx-auto">
-            <div className="px-6 my-20 flex space-x-3 md:max-w-4xl mx-auto">
-                <h3 className="relative m-auto text-center leading-relaxed">
-                    <span className="font-extrabold	text-3xl mx-1 md:text-5xl font-bold font-gabriela">"</span>
-                        {quote}
-                    <span className="font-extrabold	text-3xl mx-1 md:text-5xl font-bold font-gabriela">"</span>
+        <>
+            <div className="px-6 mx-auto text-center md:max-w-4xl">
+                <h3 className="leading-relaxed lg:leading-loose">
+                    <span className="text-3xl mx-1 md:text-5xl font-gabriela">
+                        "{quote}"
+                    </span>
                 </h3>
             </div>
             <ArrowBounceDown scrollToTarget='#about-me' />
-        </div>
+        </>
     )
 }
 
