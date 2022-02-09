d181b8a992f4aabb61059a48008a0a768f011225
Bram <brampijper@gmail.com>
2022-02-09 14:23:40 +0100

    removed unnessary elements

--- src/components/homepage/HeaderQuote.js
+++ src/components/homepage/HeaderQuote.js
@@ -15,16 +15,14 @@ const HeaderQuote = () => {
     const data = useStaticQuery(getData);
     const { strapiHomepage: { quote } } = data
     return (
-        <>
-            <div className="px-6 mx-auto text-center md:max-w-4xl">
-                <h3 className="leading-relaxed lg:leading-loose">
-                    <span className="text-3xl mx-1 md:text-5xl font-gabriela">
-                        "{quote}"
-                    </span>
-                </h3>
-            </div>
+        <div className="px-6 mx-auto text-center md:max-w-4xl space-y-16">
+            <h3 className="leading-relaxed lg:leading-loose">
+                <span className="text-3xl mx-1 md:text-5xl font-gabriela">
+                    "{quote}"
+                </span>
+            </h3>
             <ArrowBounceDown scrollToTarget='#about-me' />
-        </>
+        </div>
     )
 }
 
