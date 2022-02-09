9d7686acf0cfaf86d66b1ebaee0b4dcb83a6fb75
Bram <brampijper@gmail.com>
2022-02-09 14:24:47 +0100

    a links are now only clickable on text

--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -6,14 +6,14 @@ import { Link } from "gatsby";
 
 const ServiceCard = ({service}) => {
     return (
-        <div className="w-full h-auto rounded-lg bg-white shadow-md relative z-10 p-8 max-w-prose overflow-hidden">
+        <div className="w-full h-auto rounded-lg bg-white shadow-md relative z-10 p-8 lg:p-12 max-w-prose overflow-hidden">
             
             {/* <ServiceCardBuyContent price={service.price} /> */}
             
-            <article className="md:mt-0 md:space-y-8 space-y-4 z-20 relative md:mr-14">
+            <article className="md:mt-0 md:space-y-8 space-y-4 z-20 relative md:mr-14 lg:mr-4">
                 <h4 className="word-spacing-wide md:word-spacing-none">{service.title}</h4>                                
                 <p>{service.description}</p>
-                <Link className="underline block" to={`/services#${service.id}`}>Explore {service.title}</Link>
+                <Link className="underline inline-block" to={`/services#${service.id}`}>Explore {service.title}</Link>
             </article>
         </div>
     )
