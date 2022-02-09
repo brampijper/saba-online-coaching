4b4c388c6aa75e2f25ce684efaf8598b6e70f480
Bram <brampijper@gmail.com>
2022-02-09 17:21:22 +0100

    show prices on service cards

--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from "gatsby";
 
-// import ServiceCardBuyContent from "./ServiceCardBuyContent";
+import ServiceCardBuyContent from "./ServiceCardBuyContent";
 
 
 const ServiceCard = ({service}) => {
     return (
         <div className="w-full h-auto rounded-lg bg-white shadow-md relative z-10 p-8 lg:p-12 max-w-prose overflow-hidden">
             
-            {/* <ServiceCardBuyContent price={service.price} /> */}
+            <ServiceCardBuyContent price={service.price} />
             
             <article className="md:mt-0 md:space-y-8 space-y-4 z-20 relative md:mr-14 lg:mr-4">
                 <h4 className="word-spacing-wide md:word-spacing-none">{service.title}</h4>                                
--- src/components/service/ServiceCardBuyContent.js
+++ src/components/service/ServiceCardBuyContent.js
@@ -6,17 +6,16 @@ import CornerSvg from "../../images/svg/service-card-triangle.svg";
 
 const ServiceCardBuyContent = ({price}) => {
     return (
-        <div className="flex justify-center absolute right-0 top-0 h-auto bg-s-turquoise w-full md:block md:bg-transparent md:w-4/5">
-            <button className="flex flex-row text-center text-white relative z-10 items-center justify-center h-16 space-x-1 md:mt-6 md:flex-col md:w-32 md:float-right">
-                <span>
-                    {`€${price}`}
-                </span>
-                <span className="mr-4"></span>
-                {/* <PaylpalButton key={price} price={price} className='md:mt-2 md:mx-auto' /> */}
-                <FontAwesomeIcon icon={faShoppingCart} className='fa-lg fa-fw md:mt-2 md:mx-auto' />
-            </button> 
-        <CornerSvg className="hidden md:block w-120 h-120 absolute right-0 top-0" />
-    </div>
+        <div className="absolute right-0 top-0">
+                <button className="flex flex-col gap-2 text-center text-white absolute right-0 items-center mt-4 z-10 h-16 w-20">
+                    <span>
+                        {`€${price}`}
+                    </span>
+                    {/* <PaylpalButton key={price} price={price} className='md:mt-2 md:mx-auto' /> */}
+                    <FontAwesomeIcon icon={faShoppingCart} className='fa-lg fa-fw md:mt-2 md:mx-auto' />
+                </button> 
+                <CornerSvg className="w-80 h-80" />
+        </div>
     )
 }
 
