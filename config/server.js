2fce847280333394e573e650b668e0e0f5b1bef7
Bram <brampijper@gmail.com>
2022-09-28 15:13:16 +0200

    bug fix

--- config/server.js
+++ config/server.js
@@ -1,11 +1,16 @@
-const cred = require("../credentials.js");
+if (process.env.NODE_ENV !== 'production') {
+  const cred = require("../credentials.js");
+  const authSecret = { 'secret': cred.ADMIN_JWT_SECRET }
+} else {
+  authSecret = { 'secret': null };
+}
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', cred.ADMIN_JWT_SECRET),
+      secret: env('ADMIN_JWT_SECRET', authSecret.secret),
     },
   },
 });
