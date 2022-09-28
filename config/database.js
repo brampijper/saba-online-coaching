b423b024ccc346ebf5888a80a6bf44fb7ffa4826
Bram <brampijper@gmail.com>
2022-09-28 15:16:20 +0200

    removed cred obj

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ if (process.env.NODE_ENV === 'production') {
           client: 'postgres',
           host: env('DATABASE_HOST', 'localhost'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', cred.DATABASE_NAME),
-          username: env('DATABASE_USERNAME', cred.DATABASE_USERNAME),
-          password: env('DATABASE_PASSWORD', cred.DATABASE_PASSWORD),
+          database: env('DATABASE_NAME'),
+          username: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
           ssl: env.bool('DATABASE_SSL', false),
         },
         options: {}
