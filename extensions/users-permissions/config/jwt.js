30678323bf1e3c39a5219c4914bdfe40d8b029db
Bram <brampijper@gmail.com>
2022-09-28 15:24:27 +0200

    bug fix

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,10 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'ffd5aeb4-39d8-4239-a7ab-29128767107b'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    jwtSecret: process.env.JWT_SECRET 
+  } 
+} else {
+  const cred = require("../credentials.js");
+  module.exports = {
+    jwtSecret: cred.JWT_SECRET
+  } 
+}
