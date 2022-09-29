03c4c1e06c4866e4a69972695d18a310b56f25c4
Bram <brampijper@gmail.com>
2022-09-29 13:57:18 +0200

    simplified logic

--- server/config/server.js
+++ server/config/server.js
@@ -1,16 +1,11 @@
-if (process.env.NODE_ENV !== 'production') {
-  const cred = require("../credentials.js");
-  const authSecret = { 'secret': cred.ADMIN_JWT_SECRET }
-} else {
-  authSecret = { 'secret': null };
-}
+const cred = process.env.NODE_ENV === 'production' ? null : require("../credentials.js");
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', authSecret.secret),
+      secret: env('ADMIN_JWT_SECRET', cred.ADMIN_JWT_SECRET),
     },
   },
 });
