76f2e2599e130b2765f9d8aa7592f1ec9f8d6da9
Bram <brampijper@gmail.com>
2022-10-03 15:32:42 +0200

    added local env variables

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,4 @@
 
-if (process.env.NODE_ENV === 'production') {
   module.exports = ({ env }) => ({
     defaultConnection: 'default',
     connections: {
@@ -7,51 +6,14 @@ if (process.env.NODE_ENV === 'production') {
         connector: 'bookshelf',
         settings: {
           client: 'postgres',
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
+          host: env('PGHOST', 'localhost'),
+          port: env.int('PGPORT', 5432),
+          database: env('PGDATABASE', 'strapi-sofia-k'),
+          username: env('PGUSER', 'postgres'),
+          password: env('PGPASSWORD', 'postgres'),
           ssl: env.bool('DATABASE_SSL', false),
         },
         options: {}
       },
     },
   });
-
-} else {
-  const cred = require("../credentials.js");
-  module.exports = () => ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: 'localhost',
-          port: 5432,
-          database: cred.DATABASE_NAME,
-          username: cred.DATABASE_USERNAME,
-          password: cred.DATABASE_PASSWORD,
-          ssl: false,
-        },
-        options: {}
-      },
-    },
-  });
-}
-
-
-// if (process.env.NODE_ENV === 'production') {
-//     module.exports = {
-//       provider: 'cloudinary',
-//       providerOptions: {
-//         cloud_name: process.env.CLOUDINARY_NAME,
-//         api_key: process.env.CLOUDINARY_KEY,
-//         api_secret: process.env.CLOUDINARY_SECRET
-//       }
-//     };
-//   } else {
-//     // to use the default local provider you can return an empty configuration
-//     module.exports = {};
-//   }
