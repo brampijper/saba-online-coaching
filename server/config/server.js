4d5a9e60e06f48c45d5e15e7ad4d503b6dd56668
Bram <brampijper@gmail.com>
2022-10-03 15:32:50 +0200

    added local vars

--- server/config/server.js
+++ server/config/server.js
@@ -1,8 +1,8 @@
 const cred = process.env.NODE_ENV === 'production' ? { ADMIN_JWT_SECRET: ""} : require("../credentials.js");
 
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', 'localhost'),
+  port: env.int('PORT', 7807),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', cred.ADMIN_JWT_SECRET),
