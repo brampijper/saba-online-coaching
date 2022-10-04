128892980cc1d1bde2370df893d4f23c970c95b8
Bram <brampijper@gmail.com>
2022-10-04 14:24:14 +0200

    add src and v3

---
+++ server/src/api/contactbar/services/contactbar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contactbar service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contactbar.contactbar');
