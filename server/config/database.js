1fbdbef28325073f1601fcb612a950d11465b98f
Bram <brampijper@gmail.com>
2023-01-06 14:30:34 +0100

    updated db variables

--- server/config/database.js
+++ server/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'password'),
       schema: env('DATABASE_SCHEMA'), // Not required
       ssl: env.bool('DATABASE_SSL', false),
     },
