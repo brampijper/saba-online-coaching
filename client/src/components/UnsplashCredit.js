e6286cfe87f7e259ed4686b1e31784e00c3bb0e9
Bram <brampijper@gmail.com>
2023-01-27 20:43:37 +0100

    added spacings

--- client/src/components/UnsplashCredit.js
+++ client/src/components/UnsplashCredit.js
@@ -3,11 +3,11 @@ import React from 'react';
 const UnsplashCredit = ({textColor, className, photographer, unsplashName}) => {
     const url = `https://unsplash.com/${unsplashName}?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`
     return (
-        <div className={`text-xss text-black font-averta ${textColor} ${className}`}>
+        <div className={`text-xss font-averta ${textColor} ${className}`}>
             Photo by
-            <a href={url} target="_blank" rel="noreferrer">{` ${photographer} `}</a>
+            <a href={url} target="_blank" rel="noreferrer">{`\xa0${photographer}\xa0`}</a>
             on
-            <a href={url} target="_blank" rel="noreferrer">{` Unsplash.`}</a>
+            <a href={url} target="_blank" rel="noreferrer">{`\xa0Unslpash`}</a>
         </div>
     )
 }
