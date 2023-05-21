ddaea2e58a6792003579f5f0e6af146ec5580518
Bram <brampijper@gmail.com>
2023-05-21 14:16:53 +0200

    restructured css & html

--- client/src/components/homepage/AboutMe.js
+++ client/src/components/homepage/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import ReactMarkdown from 'react-markdown';
 
@@ -62,34 +62,44 @@ const AboutMe = () => {
     return (
         <div className="relative overflow-hidden md:overflow-visible">
             <div id="about-me" 
-            className="h-auto space-y-20 mx-auto
-                px-6 pb-8 mx-auto max-w-screen-lg 
-                md:grid md:grid-cols-2 md:gap-8 md:space-y-40
-                xl:max-w-screen-xl"
+                className="h-auto space-y-20 mx-auto
+                    px-6 pb-8 mx-auto max-w-screen-lg 
+                    md:grid md:grid-cols-2 md:gap-8 md:space-y-16
+                    xl:max-w-screen-xl"
             >
-
                 <CircleTitle title={aboutme_title} styles="col-span-2" />  
-                <div>
-                <ReactMarkdown children={aboutme_text} className="space-y-6 max-w-prose pb-4" />
-                    <Link className="underline" to='/about'>
-                        Read more about Saba Feronah
-                    </Link>
-                </div>
+                
+                <ReactMarkdown 
+                    className="space-y-6 max-w-prose pb-4" 
+                    linkTarget="_blank"
+                    children={aboutme_text} 
+                />
 
-                <div className="space-y-6 place-self-center">
-                    <div className="relative">
-                        <GatsbyImage image={image} alt={alt} className="rounded-lg max-w-sm self-center" />
-                        <UnsplashCredit 
-                            photographer="Gabnrielle Clare Marino" 
-                            unsplashName="@gabiontheroad"
-                            className="absolute inset-x-1 bottom-2" />  
-                    </div>
-                    <ReactMarkdown children={aboutme_image_text} className="space-y-2 max-w-prose md:w-2/3" />
+                <div className="place-self-center">
+                    <GatsbyImage 
+                        image={image} 
+                        alt={alt}
+                        objectFit="cover"
+                        className="rounded-lg self-center max-h-80" 
+                    />
+                    
+                    <UnsplashCredit 
+                        photographer="Moritz Kindler" 
+                        unsplashName="@moritz_photography"
+                        className="mt-0 text-center" 
+                    />  
+
+                    <ReactMarkdown 
+                        children={aboutme_image_text} 
+                        className="space-y-2 max-w-prose mt-8" 
+                    />
                 </div>
             </div>
+
             <div className="flex justify-center">
                 <Squares className="absolute z-minus top-1/15 md:top-0 w-full h-auto max-w-screen-xl" />
             </div>
+            
             <GreenStripe className="absolute z-minus top-1/4 md:top-1/3 w-auto h-full lg:w-full lg:h-auto" />
         </div>
     )
