12c072cad7d0aa9cbb14f7f290fecc2819d89502
Bram <brampijper@gmail.com>
2023-05-25 16:45:15 +0200

    added borders and adjusted spacings

--- client/src/components/homepage/Header.js
+++ client/src/components/homepage/Header.js
@@ -50,33 +50,35 @@ const Header = () => {
         <div className="bg-s-purple h-100-4 lg:h-100-6">
             
             <div className="
-                h-full grid grid-cols-1 grid-auto-rows justify-between px-6 max-w-screen-sm 
-                sm:py-0 sm:pt-0 sm:m-auto
-                md:px-0 md:grid md:grid-cols-2 md:grid-rows-3 md:max-w-screen-md
-                xl:px-12 xl:max-w-screen-lg
+                h-full grid grid-cols-1 grid-auto-rows px-6 max-w-screen-sm m-auto
+                sm:py-8 
+                md:p-0 md:grid md:grid-cols-2 md:grid-rows-auto md:max-w-screen-md
+                xl:px-8 xl:max-w-screen-lg
             ">
                 <div className="
                     w-full min-w-screen-sm border-none
-                    md:min-w-screen-md md:border-r md:col-start-1 md:col-end-3 md:row-start-1
-                    xl:min-w-screen-lg
+                    md:h-full md:flex md:justify-center md:m-auto md:col-start-1 md:col-end-3 md:row-start-1
+                    md:border-solid md:border-r-2 md:border-b-2 md:border-white
+                    
                 ">
-                    <HeaderTitle/>
+                    <HeaderTitle className="md:self-end md:max-w-[768px] xl:max-w-[868px]"/>
                 </div>
                     
                 <ReactMarkdown 
                     children={header_subtitle} 
                     className="
                         text-white flex flex-col gap-4 py-6
-                        md:gap-8 md:text-left md:text-sm md:py-0
-                        md:col-start-1 md:row-start-2 md:p-8 md:max-h-fit
+                        md:gap-8 md:text-left md:text-sm
+                        md:col-start-1 md:row-start-2 md:row-end-5 md:p-8 md:max-h-fit
                         xl:text-lg
+                        md:border-solid md:border-l-2 md:border-white
                 "/>
 
                 <ButtonScrollTo 
                     scrollToTarget='#about-me' 
                     className="
-                        text-black text-sm bg-s-turquoise row-start-5 rounded-half
-                        md:text-md md:rounded-none md:col-start-1 md:row-start-3 md:self-center md:w-full md:m-0 md:h-20
+                        text-slate-900 text-sm bg-s-turquoise row-start-5 rounded-half
+                        md:text-md md:rounded-none md:col-start-1 md:row-start-3 md:row-end-5 md:self-center md:w-full md:m-0 md:h-20
                 "/>
 
                 <GatsbyImage 
@@ -91,7 +93,7 @@ const Header = () => {
                     photographer="Sasha Freemind" 
                     unsplashName="@sashafreemind"
                     textColor="text-white"
-                    className="w-full flex justify-center md:col-start-2 md:row-start-5 md:row-end-5"
+                    className="w-full flex justify-center md:col-start-2 md:row-start-4 md:row-end-4"
                 /> 
             </div>
         </div>
