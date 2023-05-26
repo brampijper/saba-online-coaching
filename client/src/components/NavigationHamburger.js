d15914b47b79c1319af9e73d69bc0b0030df08a1
Bram <brampijper@gmail.com>
2023-05-26 23:21:24 +0200

    restructured nav hamburger icon

--- client/src/components/NavigationHamburger.js
+++ client/src/components/NavigationHamburger.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const NavigationHamburger = (props) => {
+const NavigationHamburger = ({showMenu, onClick}) => {
 
     function onIconClick() {
-        props.onClick()
+        onClick()
     }
+
+    const iconOpen = (
+        <button aria-label="open" onClick={onIconClick}>
+            <FontAwesomeIcon icon={faTimes} className='fa-2x text-white' />
+        </button>
+    )
+
+    const iconClosed = (
+        <button aria-label="close" onClick={onIconClick}>
+            <FontAwesomeIcon icon={faBars} className='fa-2x text-white' />
+        </button>
+    )
+
+    const displayIcon = showMenu ? iconOpen : iconClosed;
+
     return (
         <div className="sm:hidden z-20 absolute right-0 px-6">
-            {
-                props.showMenu 
-                    ? <button aria-label="open" onClick={onIconClick}><FontAwesomeIcon icon={faTimes} className='fa-2x text-white' /></button>
-                    : <button aria-label="close" onClick={onIconClick}><FontAwesomeIcon icon={faBars} className='fa-2x text-white' /></button>
-            }
+            {displayIcon}
         </div>
     )
 }
